a8a466c24bf6a4080070f3ddbbec00fa579af62b
Uchenna Onuigbo <uchenna.p.onuigbo@gmail.com>
2022-01-23 17:06:33 -0800

    updates

--- frmCounter.Designer.cs
+++ frmCounter.Designer.cs
@@ -41,20 +41,25 @@
             this.txtSetCount = new System.Windows.Forms.TextBox();
             this.btnResetTimer = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkDecrement = new System.Windows.Forms.CheckBox();
             this.btnClearList = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.btnPause = new System.Windows.Forms.Button();
+            this.chkAnnotateTime = new System.Windows.Forms.CheckBox();
             this.btnRemoveAnnotation = new System.Windows.Forms.Button();
+            this.btnStartTime = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveAsTextFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.loadTextFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.chkAnnotateTime = new System.Windows.Forms.CheckBox();
             this.chkMoreTools = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.save = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -69,7 +74,7 @@
             // lblTimer
             // 
             this.lblTimer.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblTimer.Location = new System.Drawing.Point(441, 21);
+            this.lblTimer.Location = new System.Drawing.Point(95, 11);
             this.lblTimer.Name = "lblTimer";
             this.lblTimer.Size = new System.Drawing.Size(105, 39);
             this.lblTimer.TabIndex = 1;
@@ -89,7 +94,7 @@
             // 
             this.txtAnnotate.Location = new System.Drawing.Point(91, 42);
             this.txtAnnotate.Name = "txtAnnotate";
-            this.txtAnnotate.Size = new System.Drawing.Size(112, 23);
+            this.txtAnnotate.Size = new System.Drawing.Size(161, 23);
             this.txtAnnotate.TabIndex = 3;
             // 
             // listAnnotations
@@ -98,7 +103,7 @@
             this.listAnnotations.ItemHeight = 15;
             this.listAnnotations.Location = new System.Drawing.Point(6, 73);
             this.listAnnotations.Name = "listAnnotations";
-            this.listAnnotations.Size = new System.Drawing.Size(206, 94);
+            this.listAnnotations.Size = new System.Drawing.Size(246, 94);
             this.listAnnotations.TabIndex = 4;
             this.listAnnotations.Tag = "Annotation List";
             // 
@@ -136,7 +141,7 @@
             // 
             // btnSetCount
             // 
-            this.btnSetCount.Location = new System.Drawing.Point(12, 153);
+            this.btnSetCount.Location = new System.Drawing.Point(12, 162);
             this.btnSetCount.Name = "btnSetCount";
             this.btnSetCount.Size = new System.Drawing.Size(104, 32);
             this.btnSetCount.TabIndex = 8;
@@ -146,9 +151,9 @@
             // 
             // btnResetAll
             // 
-            this.btnResetAll.Location = new System.Drawing.Point(242, 259);
+            this.btnResetAll.Location = new System.Drawing.Point(492, 51);
             this.btnResetAll.Name = "btnResetAll";
-            this.btnResetAll.Size = new System.Drawing.Size(86, 45);
+            this.btnResetAll.Size = new System.Drawing.Size(71, 42);
             this.btnResetAll.TabIndex = 9;
             this.btnResetAll.Text = "Reset and Clear All";
             this.btnResetAll.UseVisualStyleBackColor = true;
@@ -156,67 +161,70 @@
             // 
             // txtSetCount
             // 
-            this.txtSetCount.Location = new System.Drawing.Point(122, 162);
+            this.txtSetCount.Location = new System.Drawing.Point(122, 167);
             this.txtSetCount.MaxLength = 6;
             this.txtSetCount.Name = "txtSetCount";
             this.txtSetCount.Size = new System.Drawing.Size(45, 23);
             this.txtSetCount.TabIndex = 10;
             this.txtSetCount.Tag = "Set Count Text Box";
-            this.txtSetCount.TextChanged += new System.EventHandler(this.txtSetCount_TextChanged);
             // 
             // btnResetTimer
             // 
-            this.btnResetTimer.Location = new System.Drawing.Point(454, 63);
+            this.btnResetTimer.Location = new System.Drawing.Point(108, 53);
             this.btnResetTimer.Name = "btnResetTimer";
             this.btnResetTimer.Size = new System.Drawing.Size(83, 28);
             this.btnResetTimer.TabIndex = 11;
             this.btnResetTimer.Text = "Reset Time";
             this.btnResetTimer.UseVisualStyleBackColor = true;
+            this.btnResetTimer.Click += new System.EventHandler(this.btnResetTimer_Click);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.chkDecrement);
             this.groupBox1.Controls.Add(this.btnClearList);
             this.groupBox1.Controls.Add(this.listAnnotations);
             this.groupBox1.Controls.Add(this.chkAnnotateTime);
             this.groupBox1.Controls.Add(this.btnAnnotate);
             this.groupBox1.Controls.Add(this.btnRemoveAnnotation);
             this.groupBox1.Controls.Add(this.txtAnnotate);
-            this.groupBox1.Location = new System.Drawing.Point(334, 100);
+            this.groupBox1.Location = new System.Drawing.Point(292, 123);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(218, 200);
+            this.groupBox1.Size = new System.Drawing.Size(258, 200);
             this.groupBox1.TabIndex = 12;
             this.groupBox1.TabStop = false;
             // 
+            // chkDecrement
+            // 
+            this.chkDecrement.AutoSize = true;
+            this.chkDecrement.Location = new System.Drawing.Point(108, 17);
+            this.chkDecrement.Name = "chkDecrement";
+            this.chkDecrement.Size = new System.Drawing.Size(150, 19);
+            this.chkDecrement.TabIndex = 19;
+            this.chkDecrement.Text = "Decrement on Removal";
+            this.chkDecrement.UseVisualStyleBackColor = true;
+            // 
             // btnClearList
             // 
-            this.btnClearList.Location = new System.Drawing.Point(137, 173);
+            this.btnClearList.Location = new System.Drawing.Point(177, 173);
             this.btnClearList.Name = "btnClearList";
             this.btnClearList.Size = new System.Drawing.Size(75, 25);
             this.btnClearList.TabIndex = 18;
             this.btnClearList.Text = "Clear List";
             this.btnClearList.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // chkAnnotateTime
             // 
-            this.button1.Location = new System.Drawing.Point(352, 26);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 28);
-            this.button1.TabIndex = 13;
-            this.button1.Text = "Start Time";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // btnPause
-            // 
-            this.btnPause.Location = new System.Drawing.Point(352, 63);
-            this.btnPause.Name = "btnPause";
-            this.btnPause.Size = new System.Drawing.Size(83, 28);
-            this.btnPause.TabIndex = 14;
-            this.btnPause.Text = "Pause";
-            this.btnPause.UseVisualStyleBackColor = true;
+            this.chkAnnotateTime.AutoSize = true;
+            this.chkAnnotateTime.Location = new System.Drawing.Point(8, 17);
+            this.chkAnnotateTime.Name = "chkAnnotateTime";
+            this.chkAnnotateTime.Size = new System.Drawing.Size(104, 19);
+            this.chkAnnotateTime.TabIndex = 17;
+            this.chkAnnotateTime.Text = "Annotate Time";
+            this.chkAnnotateTime.UseVisualStyleBackColor = true;
             // 
             // btnRemoveAnnotation
             // 
-            this.btnRemoveAnnotation.Location = new System.Drawing.Point(8, 173);
+            this.btnRemoveAnnotation.Location = new System.Drawing.Point(6, 173);
             this.btnRemoveAnnotation.Name = "btnRemoveAnnotation";
             this.btnRemoveAnnotation.Size = new System.Drawing.Size(123, 25);
             this.btnRemoveAnnotation.TabIndex = 15;
@@ -224,13 +232,33 @@
             this.btnRemoveAnnotation.UseVisualStyleBackColor = true;
             this.btnRemoveAnnotation.Click += new System.EventHandler(this.btnRemoveAnnotation_Click);
             // 
+            // btnStartTime
+            // 
+            this.btnStartTime.Location = new System.Drawing.Point(6, 16);
+            this.btnStartTime.Name = "btnStartTime";
+            this.btnStartTime.Size = new System.Drawing.Size(83, 28);
+            this.btnStartTime.TabIndex = 13;
+            this.btnStartTime.Text = "Start Time";
+            this.btnStartTime.UseVisualStyleBackColor = true;
+            this.btnStartTime.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(6, 53);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(83, 28);
+            this.btnPause.TabIndex = 14;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(564, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(607, 24);
             this.menuStrip1.TabIndex = 16;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -248,61 +276,74 @@
             this.saveAsTextFileToolStripMenuItem.Name = "saveAsTextFileToolStripMenuItem";
             this.saveAsTextFileToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
             this.saveAsTextFileToolStripMenuItem.Text = "Save As Text File...";
+            this.saveAsTextFileToolStripMenuItem.Click += new System.EventHandler(this.saveAsTextFileToolStripMenuItem_Click);
             // 
             // loadTextFileToolStripMenuItem
             // 
             this.loadTextFileToolStripMenuItem.Name = "loadTextFileToolStripMenuItem";
             this.loadTextFileToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
             this.loadTextFileToolStripMenuItem.Text = "Load Text File...";
-            // 
-            // chkAnnotateTime
-            // 
-            this.chkAnnotateTime.AutoSize = true;
-            this.chkAnnotateTime.Location = new System.Drawing.Point(8, 17);
-            this.chkAnnotateTime.Name = "chkAnnotateTime";
-            this.chkAnnotateTime.Size = new System.Drawing.Size(104, 19);
-            this.chkAnnotateTime.TabIndex = 17;
-            this.chkAnnotateTime.Text = "Annotate Time";
-            this.chkAnnotateTime.UseVisualStyleBackColor = true;
+            this.loadTextFileToolStripMenuItem.Click += new System.EventHandler(this.loadTextFileToolStripMenuItem_Click);
             // 
             // chkMoreTools
             // 
             this.chkMoreTools.AutoSize = true;
-            this.chkMoreTools.Location = new System.Drawing.Point(173, 151);
+            this.chkMoreTools.Location = new System.Drawing.Point(179, 140);
             this.chkMoreTools.Name = "chkMoreTools";
-            this.chkMoreTools.Size = new System.Drawing.Size(78, 34);
+            this.chkMoreTools.Size = new System.Drawing.Size(84, 19);
             this.chkMoreTools.TabIndex = 18;
-            this.chkMoreTools.Text = "See More \r\nTools";
+            this.chkMoreTools.Text = "More Tools";
             this.chkMoreTools.UseVisualStyleBackColor = true;
             this.chkMoreTools.CheckedChanged += new System.EventHandler(this.chkMoreTools_CheckedChanged);
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(12, 207);
+            this.label2.Location = new System.Drawing.Point(12, 219);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(226, 72);
             this.label2.TabIndex = 19;
             this.label2.Text = "Clicking on the numeric textbox allows you to control the values \r\nwith your keyb" +
     "oard Up and Down Arrows respectively";
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblTimer);
+            this.groupBox2.Controls.Add(this.btnResetTimer);
+            this.groupBox2.Controls.Add(this.btnStartTime);
+            this.groupBox2.Controls.Add(this.btnPause);
+            this.groupBox2.Location = new System.Drawing.Point(291, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 91);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            // 
+            // save
+            // 
+            this.save.Filter = "Text files (*.txt)|*.txt";
+            this.save.Title = "Save as Text File";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "*.txt";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"Text files (*.txt)|*.txt";
+            // 
             // frmCounter
             // 
+            this.AcceptButton = this.btnAnnotate;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(564, 312);
+            this.ClientSize = new System.Drawing.Size(607, 348);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.chkMoreTools);
-            this.Controls.Add(this.btnPause);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.btnResetTimer);
             this.Controls.Add(this.txtSetCount);
             this.Controls.Add(this.btnResetAll);
             this.Controls.Add(this.btnSetCount);
             this.Controls.Add(this.numericUpDown);
             this.Controls.Add(this.btnResetCount);
             this.Controls.Add(this.txtReason);
-            this.Controls.Add(this.lblTimer);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -310,12 +351,12 @@
             this.MaximizeBox = false;
             this.Name = "frmCounter";
             this.Text = "Incrementer";
-            this.Load += new System.EventHandler(this.frmCounter_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -336,7 +377,7 @@
         private TextBox txtSetCount;
         private Button btnResetTimer;
         private GroupBox groupBox1;
-        private Button button1;
+        private Button btnStartTime;
         private Button btnPause;
         private Button btnRemoveAnnotation;
         private MenuStrip menuStrip1;
@@ -347,5 +388,9 @@
         private Button btnClearList;
         private CheckBox chkMoreTools;
         private Label label2;
+        private GroupBox groupBox2;
+        private CheckBox chkDecrement;
+        private SaveFileDialog save;
+        private OpenFileDialog openFileDialog1;
     }
 }
--- frmCounter.cs
+++ frmCounter.cs
@@ -1,18 +1,104 @@
+using System.Diagnostics;
+
 namespace Counter
 {
     public partial class frmCounter : Form
     {
         private const string Starting_Time = "0:00:00"; //hour, minutes, seconds
+        private readonly int FullWidth;
+        private readonly int SmallWidth;
+
+        private readonly int FullHeight;
+        private readonly int SmallHeight;
+
+        private Thread timeThread;
+        private ManualResetEvent mre;
+        private Stopwatch stopWatch;
+
+        bool isThreadRunning;
         public frmCounter()
         {
             InitializeComponent();
-            numericUpDown.Value = numericUpDown.Maximum; //testing, remove when done
+            //numericUpDown.Value = numericUpDown.Maximum; //testing, remove when done
+            FullWidth = 587;
+            SmallWidth = 303;
+
+            //set size on form start up
+            FullHeight = 374;
+            SmallHeight = 331;
+
+            Width = SmallWidth;
+            Height = SmallHeight;
+
+            DisableControls();
+
+            stopWatch = new Stopwatch();
+            isThreadRunning = false;
+
+            mre = new ManualResetEvent(false);
+            timeThread = new Thread(new ThreadStart(HandleStopWatchThread));
         }
 
         private void ResetTime()
         {
             //stop timer as well
+            if(stopWatch.IsRunning)
+            {
+                stopWatch.Reset();
+               
+                //mre.Reset();
+                //mre.Close();
+            }
             lblTimer.Text = Starting_Time;
+        }
+
+        private void HandleStopWatchThread()
+        {
+            mre.WaitOne(); //hmm...
+
+            TimeSpan timeSpan;            
+            stopWatch.Start();
+            while(isThreadRunning)
+            {
+                timeSpan = stopWatch.Elapsed;
+                if (InvokeRequired)
+                {
+                    Invoke(new MethodInvoker(() =>
+                        lblTimer.Text = String.Format("{0:0}:{1:00}:{2:00}",
+                        timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds)));
+                }
+                else
+                    lblTimer.Text = String.Format("{0:0}:{1:00}:{2:00}",
+                        timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+
+                //mre.WaitOne(); //https://stackoverflow.com/questions/10469687/how-to-pause-resume-a-thread
+            }
+        }
+
+        private void EnableControls()
+        {
+            groupBox1.Enabled = true;
+            groupBox2.Enabled = true;
+            btnResetAll.Enabled = true;
+
+            DisableTimerButtons();
+        }
+        private void DisableTimerButtons()
+        {
+            btnPause.Enabled = false;
+            btnResetTimer.Enabled = false;
+        }
+
+        private void EnableTimerButtons()
+        {
+            btnPause.Enabled = true;
+            btnResetTimer.Enabled = true;
+        }
+        private void DisableControls()
+        {
+            groupBox1.Enabled = false;
+            groupBox2.Enabled = false;
+            btnResetAll.Enabled = false;
         }
 
         private void ClearFields(bool resetAll = false, 
@@ -20,28 +106,24 @@ namespace Counter
         {
             if(resetAll)
             {
-                txtReason.Text = "";
-                ResetTime();
+                txtReason.Text = "";                
                 txtAnnotate.Text = "";
                 txtSetCount.Text = "";
+
                 listAnnotations.Items.Clear();
                 numericUpDown.Value = numericUpDown.Minimum;
+
+                ResetTime();
+                btnStartTime.Enabled = true;
+                DisableTimerButtons();
             }
             else
             {
                 if(resetCount)
                     numericUpDown.Value = numericUpDown.Minimum;
                 if(resetTime)
-                {
-                    ResetTime();
-                }
-            }
-            
-        }
-
-        private void frmCounter_Load(object sender, EventArgs e)
-        {
-
+                    ResetTime();              
+            }            
         }
 
         private void btnResetCount_Click(object sender, EventArgs e)
@@ -65,12 +147,18 @@ namespace Counter
             string text = chkAnnotateTime.Checked ? 
                 $"{numericUpDown.Value} - {lblTimer.Text} | {txtAnnotate.Text}"
                 : $"{numericUpDown.Value} - {txtAnnotate.Text}";
+
             listAnnotations.Items.Add(text);
+            txtAnnotate.Text = "";
+            if(numericUpDown.Value != numericUpDown.Maximum)
+                numericUpDown.Value++;
+
+            AutoScrollListBox();
         }
 
         private void btnResetAll_Click(object sender, EventArgs e)
         {
-            ClearFields();
+            ClearFields(true);
         }
 
         private void btnRemoveAnnotation_Click(object sender, EventArgs e)
@@ -79,18 +167,156 @@ namespace Counter
             {                
                 if (listAnnotations.SelectedItems.Count != 0)   
                     while (listAnnotations.SelectedIndex != -1)                      
-                        listAnnotations.Items.RemoveAt(listAnnotations.SelectedIndex);                                   
+                        listAnnotations.Items.RemoveAt(listAnnotations.SelectedIndex);
+
+                if (chkDecrement.Checked)
+                    if(numericUpDown.Value != numericUpDown.Minimum)
+                        numericUpDown.Value--;
             }
         }
 
         private void chkMoreTools_CheckedChanged(object sender, EventArgs e)
         {
-            //562, 325
+            if(chkMoreTools.Checked)
+            {
+                EnableControls();
+                Width = FullWidth;
+                Height = FullHeight;
+            }
+            else 
+            {
+                Width = SmallWidth;
+                Height = SmallHeight;
+                DisableControls();
+            }
         }
 
-        private void txtSetCount_TextChanged(object sender, EventArgs e)
+        //btnStartTime
+        private void button1_Click(object sender, EventArgs e)
         {
+            btnStartTime.Enabled = false;
+            EnableTimerButtons();
+            isThreadRunning = true;
+            //begin thread
+            
+            timeThread.Start();
+        }
 
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            //Pause
+            if(isThreadRunning)
+            {
+                stopWatch.Stop();
+                //mre.Reset();
+                btnPause.Text = "Continue";
+                //stop timer
+                isThreadRunning = false;
+                
+            }
+            else
+            {
+                btnPause.Text = "Pause";
+                //resume timer
+                //mre.Set();
+                isThreadRunning = true;
+                stopWatch.Start();
+
+                timeThread.Start();
+            }
+
+        }
+
+        private void btnResetTimer_Click(object sender, EventArgs e)
+        {
+            
+            DisableTimerButtons();
+            //end thread
+            stopWatch.Reset();
+            //mre.Reset();
+            //mre.Close();
+            isThreadRunning = false;
+            btnStartTime.Enabled = true;
+        }
+
+        private void SaveAsTextFile()
+        {
+            string fileName = $"{numericUpDown.Value} - {txtReason.Text}";
+            save.FileName = fileName;
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                using (FileStream S = File.Open(save.FileName, FileMode.Create))
+                {
+                    using (StreamWriter st = new StreamWriter(S))
+                    {
+                        st.WriteLine(fileName);
+                        if(chkMoreTools.Checked)
+                        {
+                            st.WriteLine(lblTimer.Text);
+                            foreach (var aa in listAnnotations.Items)
+                                st.WriteLine(aa);
+                        }
+                        
+                        st.Close();
+                    }
+                    S.Close();
+                }
+                save.FileName = "";
+            }
+        }
+
+        private void LoadTextFile()
+        {
+            string line = "";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader r = new StreamReader(openFileDialog1.FileName))
+                {
+                    if(r.ReadLine() != null)
+                    {
+                        if (chkMoreTools.Checked)
+                        {
+                            line = r.ReadLine();
+                            txtReason.Text = line;
+                            //I don't think there's a way for me to put the exact time
+                            //from the label into the Stopwatch instance
+                            r.ReadLine(); //skipping the label
+                            while (line != null)
+                            {
+                                line = r.ReadLine();
+                                if (line != null)
+                                    listAnnotations.Items.Add(line);
+                            }
+                        }
+                        else
+                        {
+                            line = r.ReadLine();
+                            txtReason.Text = line;
+                        }
+                    }
+                    r.Close();
+                }
+                openFileDialog1.FileName = "";
+            }
+        }
+
+
+        private void saveAsTextFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveAsTextFile();
+        }
+
+        private void loadTextFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadTextFile();
+        }
+
+        private void AutoScrollListBox()
+        {
+            int nItems = listAnnotations.Height / listAnnotations.ItemHeight;
+            listAnnotations.TopIndex = listAnnotations.Items.Count - nItems;
         }
     }
 }
